4591d8f9d27cebe1793f9fdc7ac0ef958d85cade
Juan Luis Barberia <50841177+jbarberia@users.noreply.github.com>
2025-03-16 15:20:08 -0300

    Algoritmo DFS

--- src/tree.py
+++ src/tree.py
@@ -1,6 +1,6 @@
 def bfs(root):
     """Recorrido del arbol de posibles estados del Sokoban a traves de
-    Breadth First Search.
+    Breadth First Search. Busqueda en el arbol primero hacía los costados.
 
     Solo devuelve una solución posible. No la óptima.
 
@@ -14,6 +14,35 @@ def bfs(root):
     queue.append(root)
     while queue:
         current = queue.pop(0)
+        if current.is_finished():
+            print("Found solution")
+            break
+
+        for move in current.get_possible_moves():
+            new_state = move()
+
+            if new_state and not new_state.is_deadlocked():
+                queue.append(new_state)
+
+    return current
+
+
+def dfs(root):
+    """Recorrido del arbol de posibles estados del Sokoban a traves de
+    Depth First Search. Busqueda en el arbol primero hacía abajo.
+
+    Solo devuelve una solución posible. No la óptima.
+
+    Args:
+        root (Sokoban): estado inicial del juego.
+
+    Returns:
+        Sokoban: estado final del juego.
+    """
+    queue = []
+    queue.append(root)
+    while queue:
+        current = queue.pop()  # En DFS se toma el primer elemento que entro
         if current.is_finished():
             print("Found solution")
             break
--- test/test_tree.py
+++ test/test_tree.py
@@ -1,5 +1,5 @@
 from src.sokoban import Sokoban
-from src.tree import bfs
+from src.tree import bfs, dfs
 
 basic_grid_with_objective = """
 #######
@@ -13,5 +13,12 @@ def test_bfs():
     s_init = Sokoban()
     s_init.parse_grid(basic_grid_with_objective)
     s_finished = bfs(s_init)    
+    assert s_finished.is_finished()
+    assert s_finished.movements == "rrrd"
+
+def test_dfs():
+    s_init = Sokoban()
+    s_init.parse_grid(basic_grid_with_objective)
+    s_finished = dfs(s_init)    
     assert s_finished.is_finished()
     assert s_finished.movements == "rrrd"
