7648566341891b0215bc123dd053f5e0d9e013e5
Juan Luis Barberia <50841177+jbarberia@users.noreply.github.com>
2025-03-18 22:01:00 -0300

    dfs con nodos visitados

--- src/tree.py
+++ src/tree.py
@@ -57,16 +57,24 @@ def dfs(root):
     """
     root.movements = ""
     queue = [root]
+    visited_nodes = []
     while queue:
         current = queue.pop()  # En DFS se toma el primer elemento que entro
+        visited_nodes.append(current)
+        print("explorando nivel {}\tmovimiento:{}".format(len(current.movements), current.movements))
         if current.is_finished():
             print("Found solution")
             break
 
         for move in current.get_possible_moves():
             new_state = move()
-
-            if new_state and not new_state.is_deadlocked():
-                queue.append(new_state)
-
+            
+            if not new_state: continue
+            if new_state.is_deadlocked(): continue
+                
+            was_visited = any([equal_nodes(new_state, prev_state) for prev_state in visited_nodes])
+            if was_visited: continue
+            
+            queue.append(new_state)
+            
     return current
